6246457b41b78692e79afa2ccf6b054d44a97c9a
SuryaSishwik <152955266+SuryaSishwik@users.noreply.github.com>
2024-06-22 20:55:13 +0530

    Create Solution.c

---
+++ function replaceCurrentDigitWithNextDigit/Solution.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int replaceCurrentDigitWithNextDigit(int n)
+{
+    int rev = 0, flag = n, count = 0;
+    while (n>0)
+    {   
+        rev = (rev * 10) + 1 ;
+        count++;
+        if (count > 1){
+            rev=rev- 1 ;
+        }
+        if(n % 10 == 9)
+        {
+            printf("%d ",rev+flag-10);
+        }
+        else
+        {
+        printf("%d ",rev+flag);
+        }
+	    n = n / 10;
+    }
+    
+}
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    replaceCurrentDigitWithNextDigit(N) ;
+    return 0;
+}
